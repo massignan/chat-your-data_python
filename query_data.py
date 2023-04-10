cd2ecbb4849461be3752725caff8cc39d581228f
Joao Paulo Massignan <massignan@gmail.com>
2023-04-10 14:34:59 -0300

    Update query_data.py
    Tunning the pompts.

--- query_data.py
+++ query_data.py
@@ -3,7 +3,7 @@ from langchain.llms import OpenAI
 from langchain.chains import ChatVectorDBChain
 
 
-_template = """Given the following conversation and a follow up question, reprhase the follow up question to be a standalone question.
+_template = """Given the following conversation and a follow up question, reprhase the follow up question to be a standalone question. You can assume the question about world economy.
 
 Chat History: {chat_history}
 Follow Up Input: {question}
@@ -11,10 +11,10 @@ Standalone question: """
 
 CONDENSATE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-template = """You are an AI assitant for answering question.
-You ar given a context and a question. Provide conversational answer.
+template = """You are an AI assitant for answering question about world economy.
+You are given the following extracted parts of a long document and a question. Provide conversational answer.
 If you don't know the answer, just say "Hmm, I am not sure." Don't try make up an answer.
-If the question is not about the context provided, politely inform that you are tuned only to answer questions about the context that you were trained.
+If the question is not about the world economy, politely inform them that you are tuned to only answer questions about world economy.
 Question: {question}
 =========
 {context}
